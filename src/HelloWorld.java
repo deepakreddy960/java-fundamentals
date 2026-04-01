730d7fb9e04222f9261455bb70ce159ec58fe274
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-04-01 15:15:38 +0530

    feat: new class Greeting added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 }
 class Greeting {
     public static void main(String[] args) {
-        System.out.println("Hello, Shiva");
+        System.out.println("Hello, Shiva prasad");
     }
 }
