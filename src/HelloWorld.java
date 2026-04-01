9b5c8497c091bd3a2b23714c89f8708de510afcc
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-04-01 15:07:30 +0530

    initial-commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    static void main() {
+        System.out.println("Hello, Welcome to Java");
+    }
+}
+class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello, Shiva");
+    }
+}
